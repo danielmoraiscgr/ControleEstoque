89ada6598aa6e68775b6ac2001232cb515008d23
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2020-07-03 21:22:07 -0300

    Controle estoque com login e menus

---
+++ ControleEstoque.Web/Controllers/ContaController.cs
@@ -0,0 +1,59 @@
+﻿using ControleEstoque.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ControleEtoque.Web.Controllers
+{
+    public class ContaController : Controller
+    {
+        [AllowAnonymous] // Tornar o metodo publico
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl; // quando o usuario fizer acesso a URL privada, vai constar aqui e vai ser redirecionado a tela de login e quando se logar vai voltar para a URL privada
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]        
+        public ActionResult Login(LoginViewModel login, string returnUrl)
+        {
+            if (!ModelState.IsValid)  // validar se a entrada do usuario esta correto
+            {
+                return View(login);
+            }
+
+            var achou = (UsuarioModel.ValidarUsuario(login.Usuario,login.Senha));
+
+            if (achou)
+            {
+                FormsAuthentication.SetAuthCookie(login.Usuario, login.LembrarMe);
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login inválido. ");
+            }
+            return View(login);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home"); 
+        }
+
+    }
+}
---
+++ ControleEstoque.Web/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEtoque.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+          
+            return View();
+        }
+
+       
+    }
+}
